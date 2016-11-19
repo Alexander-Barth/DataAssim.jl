bced0bc7f6c48fe4f1deea0e0d8f816d3894e601
Alexander Barth <barth.alexander@gmail.com>
2016-11-19 22:12:51 +0100

    initial import

--- src/DataAssim.jl
+++ src/DataAssim.jl
@@ -1,5 +1,345 @@
 module DataAssim
 
-# package code goes here
+export sangoma_ensemble_analysis, sangoma_check
 
-end # module
+function sangoma_ensemble_analysis(Xf,H,y,R,method; debug = false, tolerance=1e-10, HXf=[])
+
+tol = tolerance
+
+# ensemble size
+N = size(Xf,2)
+
+# number of observations
+m = size(y,1)
+
+xf = mean(Xf,2)
+Xfp = Xf - repmat(xf,1,N)
+
+# do not use isempty here because m might be zero
+if HXf == []
+    HXf = H*Xf
+end
+
+Hxf = mean(HXf,2)
+S = HXf - repmat(Hxf,1,N)
+
+F = S*S' + (N-1) * R
+
+if debug
+  Pf = (Xfp * Xfp') / (N-1)
+  HPfH = (S * S') / (N-1)
+  PfH = (Xfp * S') / (N-1)
+
+  K = PfH * inv(HPfH + R)
+end
+
+if method == "EnSRF"
+    # EnSRF
+    Lambda_S,Gamma_S = eig(F)
+    Lambda_S = diagm(Lambda_S)
+    X_S = S'*Gamma_S * sqrt(inv(Lambda_S))
+    U_S,Sigma_S,Z_S = svd(X_S)
+
+    Sigma_S = diagm(Sigma_S)
+
+    Xap = Xfp * (U_S * (sqrt(eye(N)-Sigma_S*Sigma_S') * U_S'))
+    xa = xf + Xfp * (S' * (Gamma_S * (Lambda_S \ (Gamma_S' * (y - Hxf)))))
+
+elseif method == "serialEnSRF"
+  # EnSRF with serial observation processing
+  for iobs = 1:m
+     Hloc = H[iobs,:]
+     yloc = y[iobs]
+     Sloc = Hloc*Xfp
+     Hxfloc = Hloc*xf
+     # ()[1] makes a scalar instead of a vector of size 1
+     Floc = (Sloc*Sloc')[1] + (N-1)*R[iobs, iobs]
+
+     Kloc = Xfp*Sloc' / Floc
+     xa = xf + Kloc * (yloc - Hxfloc)
+     alpha = 1.0 / (1.0 + sqrt( (N-1)*R[iobs,iobs]/Floc) )
+     Xap = Xfp - alpha * Kloc * Hloc * Xfp
+
+     Xfp = Xap
+     xf = xa
+  end
+
+elseif method == "ETKF"
+  # ETKF with decomposition of Stilde
+  sqrtR = sqrtm(R)
+  Stilde = sqrt(1/(N-1)) * (sqrtR \ S)
+
+  # "economy size" SVD decomposition
+  U_T,Sigma_T,V_T = svd(Stilde')
+  Sigma_T = diagm(Sigma_T)
+
+  if size(Sigma_T,2) > N
+    Sigma_T = Sigma_T(:,1:N)
+    V_T = V_T(:,1:N)
+  end
+  Ndim = size(Sigma_T,1)
+
+  TTt = eye(N) - S'*(F\S)
+
+  if debug
+    sangoma_check(TTt,U_T * ((eye(Ndim)+Sigma_T*Sigma_T') \ U_T'),
+        "ETKF-TTt",tol)
+
+    K2 = 1/sqrt(N-1) * Xfp *
+        (Stilde' * ((Stilde*Stilde'+ eye(m)) \ inv(sqrtR)))
+    sangoma_check(K,K2,"ETKF-Kalman gain",tol)
+  end
+
+  Xap = Xfp * (U_T * (sqrt(eye(Ndim)+Sigma_T*Sigma_T') \ U_T'))
+  xa = xf + 1/sqrt(N-1) *  Xfp * (U_T * ((eye(Ndim)+Sigma_T'*Sigma_T) \
+      (Sigma_T * V_T' * (sqrtR \ (y - Hxf)))))
+
+elseif method == "ETKF2"
+  # ETKF with square-root of invTTt (e.g. Hunt et al., 2007)
+
+  invR_S = R \ S
+  invTTt = (N-1) * eye(N) + S' * invR_S
+
+  # eig is more precise if the matrix is exactly symmetric
+  invTTt = (invTTt + invTTt')/2
+
+  Sigma_T,U_T = eig(invTTt)
+  Sigma_T = diagm(Sigma_T)
+
+  if debug
+    sangoma_check(U_T*Sigma_T * U_T',invTTt,"ETKF2-eig",tol)
+  end
+
+  T = U_T * (sqrt(Sigma_T) \ U_T')
+
+  if debug
+    sangoma_check(T*T,inv(invTTt),"ETKF2-sym. square root",tol)
+  end
+
+  Xap = sqrt(N-1) * Xfp * T
+  xa = xf + Xfp * (U_T * (inv(Sigma_T) * U_T' * (invR_S' * (y - Hxf))))
+
+
+# elseif method == "ETKF3"
+#   # ETKF in style of ESTKF
+#   A_T = zeros(N,N)
+
+#   for j = 1:N
+#     for i = 1:N
+#       if i == j
+#         A_T(i,j) = 1 - 1/N
+#       else
+#         A_T(i,j) = - 1/N
+#       end
+#     end
+#   end
+
+#   L = Xf*A_T
+#   HL = HXf*(A_T*L)
+
+#   if debug
+#     Pf2 = 1/(N-1) * L * ((A_T'*A_T) \ L')
+#     sangoma_check(Pf,Pf2',"ETKF3-Pf",tol)
+#     Pf2 = 1/(N-1) * L * L'
+#     sangoma_check(Pf,Pf2',"ETKF3-Pf2",tol)
+#   end
+
+#   invTTt = (N-1)*eye(N) + HL' * (R \ HL)
+
+#   Sigma_T,U_T = eig(invTTt)
+#   Sigma_T = diagm(Sigma_T)
+
+#   T = U_T * (sqrt(Sigma_T) \ U_T')
+#   #T = sqrtm(inv(invTTt))
+
+#   if debug
+#     sangoma_check(T*T,inv(invTTt),"ETKF3-sym. square root",tol)
+#   end
+
+#   Xap = sqrt(N-1) * L*T * A_T'
+#   xa = xf + L * (U_T * (inv(Sigma_T) * U_T' * (HL' * (R \ (y - Hxf)))))
+
+elseif method == "EAKF"
+  # EAKF
+  sqrtR = sqrtm(R)
+
+  Stilde = sqrt(1/(N-1)) * (sqrtR \ S)
+
+  # Sigma_A should have the size (N-1) x (N-1)
+  U_A,Sigma_A,V_A = svd(Stilde')
+  Sigma_A = diagm(Sigma_A)
+
+  if debug
+    # last singular should be zero
+    sangoma_check(abs(Sigma_A[N,N]),0,"EAKF-sigma",tol)
+  end
+
+  U_A = U_A[:,1:N-1]
+  Sigma_A = Sigma_A[1:N-1,1:N-1]
+  V_A = V_A[:,1:N-1]
+
+  # eigenvalue decomposition of Pf
+  # Gamma_A should have the size (N-1) x (N-1)
+
+  #[Z_A,Gamma_A] = eig(Pf)
+  Z_A,sqrtGamma_A,dummy = svd(Xfp)
+  sqrtGamma_A = diagm(sqrtGamma_A)
+
+  if debug
+    # last eigenvalue should be zero
+    sangoma_check(abs(sqrtGamma_A[end,end]),0,"EAKF-gamma",tol)
+  end
+
+  Gamma_A = sqrtGamma_A.^2 / (N-1)
+  Gamma_A = Gamma_A[1:N-1,1:N-1]
+  Z_A = Z_A[:,1:N-1]
+
+  if debug
+    sangoma_check(Z_A * Gamma_A * Z_A',Pf,"EAKF-decomposition",tol)
+  end
+
+  Xap = 1/sqrt(N-1) * Xfp * (U_A * ((sqrt(eye(N-1) + Sigma_A'*Sigma_A)) \
+      (sqrt(inv(Gamma_A)) * (Z_A' * Xfp))))
+
+  xa = xf + 1/sqrt(N-1) *  Xfp * (U_A * ((eye(N-1) + Sigma_A'*Sigma_A) \
+      (Sigma_A * V_A' * (sqrtR \ (y - Hxf)))))
+
+elseif method == "SEIK"
+  # SEIK
+
+  A = zeros(N,N-1)
+  A[1:N-1,1:N-1] = eye(N-1)
+  A = A - ones(N,N-1)/N
+
+  L = Xf*A
+  HL = HXf*A
+
+  if debug
+    sangoma_check(L,Xfp[:,1:N-1],"SEIK-L matrix",tol)
+    Pf2 = 1/(N-1) * L * ((A'*A) \ L')
+    sangoma_check(Pf,Pf2',"SEIK-Pf",tol)
+  end
+
+  invTTt = (N-1)*(A'*A) + HL' * (R \ HL)
+  TTt = inv(invTTt)
+
+  T = chol(inv(invTTt))'
+
+  if debug
+    sangoma_check(T*T',inv(invTTt),"SEIK-Cholesky decomposition",tol)
+  end
+
+  # add omega
+  w = ones(N,1)/sqrt(N)
+  Omega = householder(w)
+  Xap = sqrt(N-1) * L*T*Omega'
+
+  xa = xf + L * (TTt * (HL' * (R \ (y - Hxf))))
+elseif method == "ESTKF"
+  # ESTKF
+  A = zeros(N,N-1)
+
+  for j = 1:N-1
+    for i = 1:N
+      if i == j && i < N
+        A[i,j] = 1 - 1/N * 1/(1/sqrt(N)+1)
+      elseif i < N
+        A[i,j] = - 1/N * 1/(1/sqrt(N)+1)
+      else
+        A[i,j] = - 1/sqrt(N)
+      end
+    end
+  end
+
+  L = Xf*A
+  HL = HXf*A
+
+  if debug
+    Pf2 = 1/(N-1) * L * ((A'*A) \ L')
+    sangoma_check(Pf,Pf2',"ESTKF-Pf",tol)
+    Pf2 = 1/(N-1) * L * L'
+    sangoma_check(Pf,Pf2',"ESTKF-Pf2",tol)
+  end
+
+  invTTt = (N-1)*eye(N-1) + HL' * (R \ HL)
+
+  Sigma_E,U_E = eig(invTTt)
+  Sigma_E = diagm(Sigma_E)
+
+  T = U_E * (sqrt(Sigma_E) \ U_E')
+  #T = sqrtm(inv(invTTt))
+
+  if debug
+    sangoma_check(T*T,inv(invTTt),"ESTKF-sym. square root",tol)
+  end
+
+  Xap = sqrt(N-1) * L*T * A'
+  xa = xf + L * (U_E * (inv(Sigma_E) * U_E' * (HL' * (R \ (y - Hxf)))))
+elseif method == "EnKF"
+  # EnKF
+  sqrtR = sqrtm(R)
+
+  # perturbation of observations
+  Yp = sqrtR * randn(m,N)
+  Y = Yp + repmat(y,1,N)
+
+  U_F,Sigma_F,V_F = svd(S + Yp)
+  Sigma_F = diagm(Sigma_F)
+
+  G_F,Gamma_F,Z_F = svd(S'*(U_F*inv(Sigma_F)))
+
+  # unclear in manuscript
+  #Xap = Xfp * G_F * sqrt(eye(N)-Gamma_F*Gamma_F') * G_F'
+
+  Xa = Xf + Xfp * (S' * (U_F * (inv(Sigma_F)^2 * (U_F' * (Y-HXf)))))
+
+  xa = mean(Xa,2)
+  Xap = Xa - repmat(xa,1,N)
+else
+  error("sangoma:unknown_method")
+end
+
+Xa = Xap + repmat(xa,1,N)
+
+return Xa,xa
+
+end
+
+
+
+function householder(w)
+
+n = length(w)-1
+
+H = zeros(n+1,n)
+
+w2 = copy(w)
+w2[n+1] = w2[n+1] + sign(w[n+1])
+
+
+H = [eye(n); zeros(1,n)] - 1/(abs(w[n+1])+1) *  w2 * w[1:n]'
+
+return H
+end
+
+
+function sangoma_check(val1,val2,message,tol,expected_failure = false)
+
+rmsd = sqrt(mean((val1 - val2).^2))
+
+@printf("%40s: ",message)
+
+if rmsd < tol
+    @printf("%s (RMS = %g)\n", " OK ", rmsd)
+else
+  if expected_failure
+      @printf("%s (RMS = %g)\n", " expected failure ", rmsd)
+  else
+      @printf("%s (RMS = %g)\n", "FAIL", rmsd)
+      error("sangoma_check failed")
+  end
+end
+
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,101 @@ using DataAssim
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+
+
+
+# number of elements in the state vector
+n = 10;
+# ensemble size
+N = 3;
+# number of observations
+m = 5;
+
+# if debug is one, then internal checks are activated
+debug = false;
+debug = true;
+tol = 1e-10;
+
+y = randn(m,1);
+Xf = randn(n,N);
+
+H = randn(m,n);
+
+y = 1:m
+Xf = reshape(sin(3*(1:(n*N))),n,N);
+H = reshape(1:(m*n),m,n);
+
+R = 2*eye(m,m);
+
+xf = mean(Xf,2);
+Xfp = Xf - repmat(xf,1,N);
+
+
+Pf = (Xfp * Xfp') / (N-1);
+K = Pf * H'*inv(H*Pf*H' + R);
+Pa_check = Pf - K*H*Pf;
+xa_check = xf + K*(y - H*xf);
+
+method = ["EnSRF","EAKF","ETKF","ETKF2","SEIK","ESTKF","serialEnSRF"]
+#method = ["EnSRF","serialEnSRF","ETKF","ETKF2","EAKF","SEIK","ESTKF"]
+method = ["EnSRF","EAKF","ETKF","ETKF2","SEIK","ESTKF"]
+
+print("Non-serial algorithms (all observations at once) with H\n")
+
+for i = 1:length(method)
+  Xa,xa = sangoma_ensemble_analysis(Xf,H,y,R,method[i],
+                                    debug=debug, tolerance=tol);
+  Xap = Xa - repmat(xa,1,N);
+
+  # check results
+  sangoma_check(xa,xa_check,method[i] * "-analysis",tol)
+
+  sangoma_check(mean(Xa,2),xa_check,
+                method[i] * "-analysis ensemble mean",tol);
+
+  sangoma_check((Xap * Xap') / (N-1),Pa_check,
+                method[i] * "-analysis ensemble variance",tol)
+end
+
+# #
+method = ["EnSRF","EAKF","ETKF","ETKF2","SEIK","ESTKF"]
+#method = ["EnSRF"]
+
+
+print("Non-serial algorithms (all observations at once) with HXf\n")
+
+for i = 1:length(method)
+  Xa,xa = sangoma_ensemble_analysis(Xf,[],y,R,method[i],
+                                    debug=debug, tolerance=tol,HXf=H*Xf)
+  Xap = Xa - repmat(xa,1,N)
+
+  # check results
+  sangoma_check(xa,xa_check,method[i] * "-analysis",tol)
+
+  sangoma_check(mean(Xa,2),xa_check,
+                method[i] * "-analysis ensemble mean",tol);
+
+  sangoma_check((Xap * Xap') / (N-1),Pa_check,
+                method[i] * "-analysis ensemble variance",tol)
+end
+
+
+#   method = {"EnSRF","ETKF","SEIK","ESTKF","serialEnSRF"};
+#   print("Serial algorithms (one observation after the other)")
+
+
+# for i = 1:length(method)
+#   [Xa,xa] = sangoma_ensemble_analysis(Xf,H,y,R,method[i],
+#       "debug",0,"tolerance",tol,"serial",1);
+#   Xap = Xa - repmat(xa,[1 N]);
+
+#   % check results
+#   sangoma_check(xa,xa_check,method[i] * "-analysis",tol)
+
+#   sangoma_check(mean(Xa,2),xa_check,
+#                 method[i] * "-analysis ensemble mean",tol);
+
+#   sangoma_check((Xap * Xap') / (N-1),Pa_check,
+#                 method[i] * "-analysis ensemble variance",tol)
+# end
+
