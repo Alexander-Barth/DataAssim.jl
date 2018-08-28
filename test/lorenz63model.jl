f89cce58d3c8e4e45323c769317e491ac4ee8351
Alexander Barth <barth.alexander@gmail.com>
2018-08-28 21:02:42 +0200

    wip

--- test/lorenz63model.jl
+++ test/lorenz63model.jl
@@ -1,39 +1,84 @@
 mutable struct Lorenz63Model{T} <: AbstractModel
     dt::T
-    sigma::T
-    beta::T
-    rho::T
+    σ::T
+    β::T
+    ρ::T
 end
 
 
-Lorenz63Model(dt,sigma=10.,beta = 8/3.,rho = 28.) = Lorenz63Model(dt,sigma,beta,rho)
+Lorenz63Model(dt,σ=10.,β = 8/3.,ρ = 28.) = Lorenz63Model(dt,σ,β,ρ)
 
-function (M::Lorenz63Model)(t,x,eta = zeros(eltype(x),size(x)))
-    return rungekutta2(t,x,M.dt,(t,x) -> lorenz63_dxdt(M.sigma,M.beta,M.rho,x)) + eta
+function (ℳ::Lorenz63Model)(t,x,eta = zeros(eltype(x),size(x)))
+    return rungekutta2(t,x,ℳ.dt,(t,x) -> lorenz63_dxdt(ℳ.σ,ℳ.β,ℳ.ρ,x)) + eta
 end
 
-function tgl(M::Lorenz63Model,t,x,dx)
-    f_tgl(t,x,dx)  = [  -M.sigma  M.sigma       0; M.rho-x[3]     -1   -x[1]; x[2]   x[1]   -M.beta] * dx;
-    f(t,x) = lorenz63_dxdt(M.sigma,M.beta,M.rho,x)
-    return rungekutta2_tgl(t,x,M.dt,f,dx,f_tgl);
+function tgl(ℳ::Lorenz63Model,t,x,dx)
+    f_tgl(t,x,dx)  = lorenz63_dxdt_tgl(ℳ.σ,ℳ.β,ℳ.ρ,x,dx)
+    f(t,x) = lorenz63_dxdt(ℳ.σ,ℳ.β,ℳ.ρ,x)
+    return rungekutta2_tgl(t,x,ℳ.dt,f,dx,f_tgl);
 end
 
-function adj(M::Lorenz63Model,t,x,dx)
-    f_adj(t,x,dx) = [  -M.sigma   M.sigma       0; M.rho-x[3]     -1   -x[1]; x[2]   x[1]   -M.beta]' * dx;
-    f(t,x) = lorenz63_dxdt(M.sigma,M.beta,M.rho,x)
+function adj(ℳ::Lorenz63Model,t,x,dx)
+    f_adj(t,x,dx)  = lorenz63_dxdt_adj(ℳ.σ,ℳ.β,ℳ.ρ,x,dx)
+    #f_adj(t,x,dx) = [  -ℳ.σ   ℳ.σ       0; ℳ.ρ-x[3]     -1   -x[1]; x[2]   x[1]   -ℳ.β]' * dx;
+    f(t,x) = lorenz63_dxdt(ℳ.σ,ℳ.β,ℳ.ρ,x)
 
-    return rungekutta2_adj(t,x,M.dt,f,dx,f_adj);
+    return rungekutta2_adj(t,x,ℳ.dt,f,dx,f_adj);
 end
 
-
-
-function lorenz63_dxdt(sigma,beta,rho,x)
+function lorenz63_dxdt(σ,β,ρ,x)
     dxdt = similar(x)
-    dxdt[1] = sigma*(x[2]-x[1])
-    dxdt[2] = x[1]*(rho-x[3]) - x[2]
-    dxdt[3] = x[1]*x[2] - beta * x[3]
+    dxdt[1] = σ*(x[2]-x[1])
+    dxdt[2] = x[1]*(ρ-x[3]) - x[2]
+    dxdt[3] = x[1]*x[2] - β * x[3]
     return dxdt
 end
+
+function lorenz63_dxdt_tgl(σ,β,ρ,x,dx)
+    @show "here2"
+    Ddxdt = similar(x)
+    Ddxdt[1] = σ*(dx[2]-dx[1])
+    Ddxdt[2] = dx[1]*(ρ-x[3]) - x[1]*dx[3] - dx[2]
+    Ddxdt[3] = dx[1]*x[2] + x[1]*dx[2] - β * dx[3]
+    #return [  -σ  σ       0; ρ-x[3]     -1   -x[1]; x[2]   x[1]   -β] * dx
+    return Ddxdt
+end
+
+function lorenz63_dxdt_adj2(σ,β,ρ,x,dx)
+    @show "here3"
+    Ddxdt = similar(x)
+    Ddxdt[1] = σ*(dx[2]-dx[1])
+    Ddxdt[2] = dx[1]*(ρ-x[3]) - x[1]*dx[3] - dx[2]
+    Ddxdt[3] = dx[1]*x[2] + x[1]*dx[2] - β * dx[3]
+
+    Ddxdt = [  -σ  σ       0; ρ-x[3]     -1   -x[1]; x[2]   x[1]   -β]' * dx
+#    Ddxdt[3] = dx[1]*x[2] + x[1]*dx[2] - β * dx[3]
+
+
+    return Ddxdt
+end
+
+
+function lorenz63_dxdt_adj(σ,β,ρ,x,Ddxdt)
+    @show "here4"
+    dx = similar(x)
+    #Ddxdt[1] = σ*(dx[2]-dx[1])
+    dx[1] = -σ * Ddxdt[1]
+    dx[2] = σ * Ddxdt[1]
+
+    #Ddxdt[2] = dx[1]*(ρ-x[3]) - x[1]*dx[3] - dx[2]
+    dx[1] += Ddxdt[2] * (ρ-x[3])
+    dx[3] = -Ddxdt[2] * x[1]
+    dx[2] += -Ddxdt[2]
+
+    #Ddxdt[3] = dx[1]*x[2] + x[1]*dx[2] - β * dx[3]
+    dx[1] += Ddxdt[3] * x[2]
+    dx[2] += Ddxdt[3] * x[1]
+    dx[3] += -Ddxdt[3] * β
+
+    return dx
+end
+
 
 function rungekutta2(t,x,dt,f)
     k1 = dt * f(t,x);
@@ -51,78 +96,6 @@ function rungekutta2_tgl(t,x,dt,f,Dx,Df)
     Dxn = Dx + Dk2;
     return Dxn
 end
-
-
-# function Dx = rungekutta2_adj_orig(t,x,dt,f,Dxn,Df_adj)
-
-# k1 = dt * f(t,x);
-# k2 = dt * f(t + dt/2,x + k1/2);
-# xn = x + k2;
-
-# # Dk1 = dt * Df(t,x,Dx);
-# # # Dk1  = [0   dt*Df] Dk1
-# # # Dx   = [0   1    ] Dx
-
-
-
-# # Dtmp2 = Dx + Dk1/2;
-# # # Dtmp2    [0  1 1/2] Dtmp2
-# # # Dx    =  [0  1  0 ] Dx
-# # # Dk1      [0  0   1] Dk1
-
-# # Dtmp = Df(t + dt/2,x + k1/2,Dtmp2)
-# # # Dtmp  = [0   Df] Dtmp
-# # # Dtmp2   [0   1 ] Dtmp2
-
-
-# # Dk2 = dt * Dtmp;
-# # # Dk2    = [0  dt] Dk2
-# # # Dtmp     [0  1 ] Dtmp
-
-# # Dxn = Dx + Dk2;
-# # #[0 1 1; 
-# # # 0 1 0
-# # # 0 0 1]
-
-# Dx = 0;
-# Dk1 = 0;
-# Dk2 = 0;
-# Dtmp = 0;
-# Dtmp2 = 0;
-
-# Dx = Dx + Dxn;
-# Dk2 = Dk2 + Dxn;
-# Dxn = 0;
-
-# # Dk2    = [0   0] Dk2
-# # Dtmp     [dt  1 ] Dtmp
-
-# Dtmp = dt*Dk2 + Dtmp;
-# Dk2 = 0;
-
-# # Dtmp  = [0     0 ] Dtmp
-# # Dtmp2   [Df'   1 ] Dtmp2
-
-# Dtmp2 = Df_adj(t + dt/2, x+k1/2, Dtmp) + Dtmp2;
-
-# # Dtmp2    [0  0  0 ] Dtmp2
-# # Dx    =  [1  1  0 ] Dx
-# # Dk1      [1/2 0 1 ] Dk1
-
-# Dx = Dx + Dtmp2;
-# Dk1 = Dk1 + Dtmp2/2 ;
-# Dtmp2 = 0;
-
-# # Dk1  = [0        0 ] Dk1
-# # Dx   = [dt*Df'   1 ] Dx
-
-# Dx = Dx + dt*Df_adj(t,x,Dk1);
-# Dk1 = 0;
-
-# end
-
-
-
 
 function rungekutta2_adj(t,x,dt,f,Dxn,Df_adj)
     k1 = dt * f(t,x);
