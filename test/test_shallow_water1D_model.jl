eafd272103e0cc385f457d7a74c9fd58e62b1b16
Alexander Barth <barth.alexander@gmail.com>
2018-12-04 11:06:30 +0100

    shallow water 1D

--- test/test_shallow_water1D_model.jl
+++ test/test_shallow_water1D_model.jl
@@ -8,5 +8,23 @@ L = 10000
 ℳ = LinShallowWater1DModel(dt,g,h,L,imax)
 
 
+
 n = 2*imax-1
 check(ℳ,n)
+
+ζt = exp.( - (ℳ.x_r .- L/2).^2 )
+ut = zeros(size(ℳ.x_u))
+
+xit = pack(ζt,ut)
+Pi = Diagonal(ones(n))
+
+Q = Diagonal(zeros(n))
+R = Diagonal(ones(size(ζt)))
+method = "4DVar";
+H = sparse(1:imax-1,1:imax-1,ones(imax-1),imax-1,n)
+nmax = 100
+
+no=3:5:nmax;
+
+xt,xfree,xa,yt,yo = TwinExperiment(ℳ,xit,Pi,Q,R,H,nmax,no,method);
+
