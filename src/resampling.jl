4df8dd039acdb9910fa14ba46e8634c30c5ed8d7
Alexander Barth <barth.alexander@gmail.com>
2019-07-03 22:46:18 +0200

    add resampling.jl

---
+++ src/resampling.jl
@@ -0,0 +1,60 @@
+using Statistics, Random, Test
+
+# Algorithm 1: Algorithm of probabilistic resampling (PR)
+
+function PR(w,Ñₑ)
+    Nₑ = length(w)
+    ŵ = similar(w, eltype(w), (Ñₑ,))
+    ŵ[1] = w[1]
+
+    for j = 2:Nₑ
+        ŵ[j] = ŵ[j-1] + w[j]
+    end
+
+    k = zeros(Int,Ñₑ)
+    c = 1
+    for j = 1:Ñₑ
+        u = rand()
+
+        while u > ŵ[c]
+            c = c+1
+        end
+
+        k[j] = c
+        c = 1
+    end
+    return k
+end
+
+k = PR([0,0.1,0.9],10000);
+@test sum(k .== 1) == 0
+
+# Algorithm 2 Algorithm of stochastic universal resampling (SUR)
+function SUR(w,Ñₑ)
+    Nₑ = length(w)
+
+    ŵ = similar(w, eltype(w), (Ñₑ,))
+    ŵ[1] = w[1]
+
+    for j = 2:Nₑ
+        ŵ[j] = ŵ[j-1] + w[j]
+    end
+
+    # generate a random number
+    u = rand()/Nₑ
+    c = 1
+
+    for j = 1:Ñₑ
+        while u > ŵ[c]
+            c = c+1
+        end
+
+        k[j] = c
+        u = u + 1/Nₑ
+        c = 1
+    end
+
+    return k
+end
+
+k = SUR([0,0.1,0.9],10000);
