1642023ee9bb4649df6f5176a89455b2cf3710c5
Alexander Barth <barth.alexander@gmail.com>
2022-06-20 21:50:31 +0200

    more docs

--- docs/src/index.md
+++ docs/src/index.md
@@ -5,6 +5,32 @@ Documentation for [DataAssim.jl](https://github.com/Alexander-Barth/DataAssim.jl
 ## Simulation driver
 
 
+The observations `yo` are provided as a vector of vectors (possibly of different length). Similarily their error covariance `R` is a vector of matrices.
+The elements of the vectors `yo` and `R` can be constructed as needed by the type `DataAssim.VectorFun`:
+
+```julia
+using DataAssim, LinearAlgebra
+R = DataAssim.VectorFun(Matrix{Bool},10,n -> (n < 5 : Matrix(I,3,3) : Matrix(I,5,5)))
+```
+
+The model ``ℳ`` implementes the following API:
+
+* ``ℳ(n,x)`` apply the model to x to forecast the index state vector. n is the time index.
+* ``tgl(ℳn,x,Δx)`` apply the tangent linear model for a perturbation ``Δx`` around ``x`` (foreward differentiation).
+* ``adj(ℳn,x,Δx)`` apply the adjoin model (reverse differentiation).
+
+For any ``x``, ``Δx₁``, ``Δx₂`` and ``n``, tangent linear and adjoint model are related by:
+
+```math
+Δx₂ ⋅ tgl(ℳ,n,x,Δx₁) = adj(ℳ,n,x,Δx₂) ⋅ Δx₁
+```
+
+where ⋅ is the inner product.
+
+The same API should also be implemented for the observation ``𝓗`` where ``𝓗(n,x)`` represents the observed part of the state vector.
+Note that for the Kalman Filter method the adjoint (of the model or the observation operator) is not needed.
+
+
 ```@docs
 FreeRun
 KalmanFilter
--- src/KalmanFilter.jl
+++ src/KalmanFilter.jl
@@ -3,44 +3,13 @@
 # x of size n x (nmax+1)
 
 """
-    x,P = KalmanFilter(xi,Pi,ℳ,Q,yo,R,H,nmax,no)
+    x,P = KalmanFilter(xi,Pi,ℳ,Q,yo,R,𝓗,nmax,no)
 
 Kalman Filter with the model `ℳ` and `nmax` time-steps starting at the
 initial condition `xi` and error covariance `Pi`.
 Observations `yo` (and error covariance `R`) at the time steps given in `no` are
 assimilated with the observation operator `H`.
 """
-function KalmanFilter(xi,Pi,ℳ,Q,yo::AbstractMatrix,R::AbstractMatrix,H::AbstractMatrix,nmax,no)
-
-    x = zeros(size(xi,1),nmax+1);
-    P = zeros(size(xi,1),size(xi,1),nmax+1);
-    obsindex = 1;
-
-    for n=1:nmax+1
-        if n == 1
-            # initialization
-            x[:,1] = xi;
-            P[:,:,1] = Pi;
-        else
-            # time integration
-            x[:,n] = ℳ(n-1,x[:,n-1]);
-            P[:,:,n] = tgl(ℳ,n-1,x[:,n-1],tgl(ℳ,n-1,x[:,n-1],P[:,:,n-1])') + Q;
-        end
-
-        if obsindex <= length(no) && n == no[obsindex]
-            # assimilation
-            K = P[:,:,n]*H'*inv(H*P[:,:,n]*H' + R);
-            x[:,n]  = x[:,n] + K * (yo[:,obsindex] - H*x[:,n]);
-            P[:,:,n] = P[:,:,n] - K*H*P[:,:,n];
-
-            obsindex = obsindex + 1;
-        end
-    end
-
-    return x,P
-end
-
-
 function KalmanFilter(xi,Pi,ℳ,Q,yo::Function,R::Function,𝓗::AbstractModel,nmax,no)
     T = eltype(xi)
     KalmanFilter(
