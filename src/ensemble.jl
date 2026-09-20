924d3e10eb80bfbee23026269548901f305e1fac
Alexander Barth <barth.alexander@gmail.com>
2026-09-20 13:00:46 +0200

    copy nolonger necessary

--- src/ensemble.jl
+++ src/ensemble.jl
@@ -108,8 +108,8 @@ Sangoma D3.1 http://data-assimilation.net/Documents/sangomaDL3.1.pdf
                 Stilde = sqrt(1/(N-1)) * (sqrtR \ S)
 
                 # "economy size" SVD decomposition
-                #U_T,Sigma_T,V_T = svd(Stilde')
-                U_T,Sigma_T,V_T = svd(copy(Stilde'))
+                U_T,Sigma_T,V_T = svd(Stilde')
+                #U_T,Sigma_T,V_T = svd(copy(Stilde'))
                 Sigma_T = Diagonal(Sigma_T)
 
                 if size(Sigma_T,2) > N
